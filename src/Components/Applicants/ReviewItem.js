3c40d0ee0346ab7b9a19d81515f6bd8b0a96db2d
Mohamed Saber <mohamed.71.saber@gmail.com>
2023-01-01 22:07:35 +0200

    adding-review-css

--- src/Components/Applicants/ReviewItem.js
+++ src/Components/Applicants/ReviewItem.js
@@ -5,37 +5,19 @@ const ReviewItem = ({ name, email, phone }) => {
     <>
       <div className="item-main">
         <div className="item">
-          <div>
-            <p>Name :</p>
-          </div>
-          <div>
-            <b>{name}</b>
-          </div>
+          <p>Name :</p>
+          <p><b> {name} </b></p>
         </div>
         <div className="item">
-          <div>
-            <p>Email :</p>
-          </div>
-          <div>
-            <b>{email}</b>
-          </div>
+          <p>Email :</p>
+          <p>{email}</p>
         </div>
         <div className="item">
-          <div>
-            <p>Phone number :</p>
-          </div>
-          <div>
-            <b>{phone}</b>
-          </div>
+          <p>Phone number :</p>
+          <p>{phone}</p>
         </div>
         <div className="item check">
-          <div>
-            <input type="checkbox" />{" "}
-          </div>{" "}
-          <div>
-            {" "}
-            <label>Reviewed</label>{" "}
-          </div>
+          <div><input type="checkbox" /> </div> <div> <label>Reviewed</label> </div>
         </div>
       </div>
       <hr />
